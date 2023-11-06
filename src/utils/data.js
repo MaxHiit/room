9d6cb7762d1848b1478ec8523141cc5b3b81c1ca
MaxHiit <pro.maxcode@gmail.com>
2023-11-06 17:29:28 +0100

    fix(image path): change data image path

--- src/utils/data.js
+++ src/utils/data.js
@@ -1,17 +1,17 @@
 export const sliderData = [
   {
-    imageSrc: '/src/assets/images/desktop-image-hero-1.jpg',
+    imageSrc: '/assets/images/desktop-image-hero-1.jpg',
     title: 'Discorver innovative ways to decorate',
     desc: 'We provide unmatched quality, comfort, and, style for property owners across the country. Our experts combine form and function in bringing your vision to life. Create a room in your own style with our collection and make your property a reflection of you and what you.'
   },
   {
-    imageSrc: '/src/assets/images/desktop-image-hero-2.jpg',
+    imageSrc: '/assets/images/desktop-image-hero-2.jpg',
     title: 'We are available all across the globe ',
     desc: "With stores all over the world, it's easy for you to find furniture for your home or place of business. Locally, we're in most major cities throughout the country. Find the branch nearest you using our store locator. Any question ? Don't hesitate to contact us today"
   },
   {
-    imageSrc: '/src/assets/images/desktop-image-hero-3.jpg',
+    imageSrc: '/assets/images/desktop-image-hero-3.jpg',
     title: 'Manufactured with the best materials',
     desc: 'Our modern furniture store provide a high level of quality. Our company has invested in advanced technology to ensure that every product is made as perfect and as consistent as possible. With three decades of experience in this industry, we understand what customers want for their home and office.'
   }
-]
+];
